2c2724b10f8a8bafa4f7c144f8a45169edb3e443
jessicatriana <jessica.triana.tx@gmail.com>
2020-01-30 09:59:11 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+
+attr_accessor :name
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+
+    def title=(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+
+end

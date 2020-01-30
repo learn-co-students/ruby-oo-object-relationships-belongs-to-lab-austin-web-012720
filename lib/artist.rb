bf8009ceb1026341d33024adbbe4de06cd0a1fa0
jacqueshebert <jacques.henri.hebert@gmail.com>
2020-01-30 09:54:04 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist
+
+        attr_reader :name
+    def initialize()
+
+
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+    attr_reader :title, :artist
+
+    def initialize
+
+
+    end
+    def title=(title)
+        @title = title
+    end
+    def artist=(artist)
+        @artist = artist
+    end
+end
